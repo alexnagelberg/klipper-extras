1ec1513808cc175f6639b38de39d660d1b609eef
Alexander Nagelberg <alexander.nagelberg@va.gov>
2022-07-09 22:14:23 -0500

    Added mutex to init (maybe I can remove delays after testing)

--- klippy/extras/vl6180x.py
+++ klippy/extras/vl6180x.py
@@ -129,6 +129,7 @@ class vl6180x:
 
   def __init__(self, config):
     printer = config.get_printer()
+    self.mutex = printer.get_reactor().mutex()
 
     # Get i2c channel from name
     # TODO: allow channel to be overriden by config i2c_multiplexer_index
@@ -156,78 +157,79 @@ class vl6180x:
     printer.register_event_handler("klippy:connect", self.handle_connect)
 
   def handle_connect(self):
-    self._multiplexer.i2c_write([1 << self._i2c_index])
-    time.sleep(0.1)
+    with self.mutex:
+      self._multiplexer.i2c_write([1 << self._i2c_index])
+      time.sleep(0.1)
 
-    # Init with required settings from application notes pg24
-    # https://www.st.com/resource/en/application_note/an4545-vl6180x-basic-ranging-application-note-stmicroelectronics.pdf
-    self.set_register(0x0207, 0x01)
-    self.set_register(0x0208, 0x01)
-    self.set_register(0x0096, 0x00)
-    self.set_register(0x0097, 0xfd)
-    self.set_register(0x00e3, 0x00)
-    self.set_register(0x00e4, 0x04)
-    self.set_register(0x00e5, 0x02)
-    self.set_register(0x00e6, 0x01)
-    self.set_register(0x00e7, 0x03)
-    self.set_register(0x00f5, 0x02)
-    self.set_register(0x00d9, 0x05)
-    self.set_register(0x00db, 0xce)
-    self.set_register(0x00dc, 0x03)
-    self.set_register(0x00dd, 0xf8)
-    self.set_register(0x009f, 0x00)
-    self.set_register(0x00a3, 0x3c)
-    self.set_register(0x00b7, 0x00)
-    self.set_register(0x00bb, 0x3c)
-    self.set_register(0x00b2, 0x09)
-    self.set_register(0x00ca, 0x09)
-    self.set_register(0x0198, 0x01)
-    self.set_register(0x01b0, 0x17)
-    self.set_register(0x01ad, 0x00)
-    self.set_register(0x00ff, 0x05)
-    self.set_register(0x0100, 0x05)
-    self.set_register(0x0199, 0x05)
-    self.set_register(0x01a6, 0x1b)
-    self.set_register(0x01ac, 0x3e)
-    self.set_register(0x01a7, 0x1f)
-    self.set_register(0x0030, 0x00)
+      # Init with required settings from application notes pg24
+      # https://www.st.com/resource/en/application_note/an4545-vl6180x-basic-ranging-application-note-stmicroelectronics.pdf
+      self.set_register(0x0207, 0x01)
+      self.set_register(0x0208, 0x01)
+      self.set_register(0x0096, 0x00)
+      self.set_register(0x0097, 0xfd)
+      self.set_register(0x00e3, 0x00)
+      self.set_register(0x00e4, 0x04)
+      self.set_register(0x00e5, 0x02)
+      self.set_register(0x00e6, 0x01)
+      self.set_register(0x00e7, 0x03)
+      self.set_register(0x00f5, 0x02)
+      self.set_register(0x00d9, 0x05)
+      self.set_register(0x00db, 0xce)
+      self.set_register(0x00dc, 0x03)
+      self.set_register(0x00dd, 0xf8)
+      self.set_register(0x009f, 0x00)
+      self.set_register(0x00a3, 0x3c)
+      self.set_register(0x00b7, 0x00)
+      self.set_register(0x00bb, 0x3c)
+      self.set_register(0x00b2, 0x09)
+      self.set_register(0x00ca, 0x09)
+      self.set_register(0x0198, 0x01)
+      self.set_register(0x01b0, 0x17)
+      self.set_register(0x01ad, 0x00)
+      self.set_register(0x00ff, 0x05)
+      self.set_register(0x0100, 0x05)
+      self.set_register(0x0199, 0x05)
+      self.set_register(0x01a6, 0x1b)
+      self.set_register(0x01ac, 0x3e)
+      self.set_register(0x01a7, 0x1f)
+      self.set_register(0x0030, 0x00)
 
-    # Recommended settings from application notes pg24
-    # Set GPIO1 high when sample complete
-    self.set_register(self.__VL6180X_SYSTEM_MODE_GPIO1, 0x10)
-    # Set Avg sample period
-    self.set_register(self.__VL6180X_READOUT_AVERAGING_SAMPLE_PERIOD, 0x30)
-    # Set the ALS gain
-    self.set_register(self.__VL6180X_SYSALS_ANALOGUE_GAIN, 0x46)
-    # Set auto calibration period (Max = 255)/(OFF = 0)
-    self.set_register(self.__VL6180X_SYSRANGE_VHV_REPEAT_RATE, 0xFF)
-    # Set ALS integration time to 100ms
-    self.set_register(self.__VL6180X_SYSALS_INTEGRATION_PERIOD, 0x63)
-    # perform a single temperature calibration
-    self.set_register(self.__VL6180X_SYSRANGE_VHV_RECALIBRATE, 0x01)
+      # Recommended settings from application notes pg24
+      # Set GPIO1 high when sample complete
+      self.set_register(self.__VL6180X_SYSTEM_MODE_GPIO1, 0x10)
+      # Set Avg sample period
+      self.set_register(self.__VL6180X_READOUT_AVERAGING_SAMPLE_PERIOD, 0x30)
+      # Set the ALS gain
+      self.set_register(self.__VL6180X_SYSALS_ANALOGUE_GAIN, 0x46)
+      # Set auto calibration period (Max = 255)/(OFF = 0)
+      self.set_register(self.__VL6180X_SYSRANGE_VHV_REPEAT_RATE, 0xFF)
+      # Set ALS integration time to 100ms
+      self.set_register(self.__VL6180X_SYSALS_INTEGRATION_PERIOD, 0x63)
+      # perform a single temperature calibration
+      self.set_register(self.__VL6180X_SYSRANGE_VHV_RECALIBRATE, 0x01)
 
-    ## Optional settings from application notes pg 25
-    ## Set default ranging inter-measurement period to 100ms
-    self.set_register(self.__VL6180X_SYSRANGE_INTERMEASUREMENT_PERIOD, 0x09)
-    ## Set default ALS inter-measurement period to 100ms
-    self.set_register(self.__VL6180X_SYSALS_INTERMEASUREMENT_PERIOD, 0x31)
-    ## Configures interrupt on 'New Sample Ready threshold event' 
-    self.set_register(self.__VL6180X_SYSTEM_INTERRUPT_CONFIG_GPIO, 0x24)
+      ## Optional settings from application notes pg 25
+      ## Set default ranging inter-measurement period to 100ms
+      self.set_register(self.__VL6180X_SYSRANGE_INTERMEASUREMENT_PERIOD, 0x09)
+      ## Set default ALS inter-measurement period to 100ms
+      self.set_register(self.__VL6180X_SYSALS_INTERMEASUREMENT_PERIOD, 0x31)
+      ## Configures interrupt on 'New Sample Ready threshold event' 
+      self.set_register(self.__VL6180X_SYSTEM_INTERRUPT_CONFIG_GPIO, 0x24)
 
-    ## Additional settings defaults from community
-    #self.set_register(self.__VL6180X_SYSRANGE_MAX_CONVERGENCE_TIME, 0x32)
-    #self.set_register(
-    #    self.__VL6180X_SYSRANGE_RANGE_CHECK_ENABLES, 0x10 | 0x01)
-    #self.set_register_16bit(
-    #    self.__VL6180X_SYSRANGE_EARLY_CONVERGENCE_ESTIMATE, 0x7B)
-    #self.set_register_16bit(self.__VL6180X_SYSALS_INTEGRATION_PERIOD, 0x64)
-    #self.set_register(self.__VL6180X_SYSALS_ANALOGUE_GAIN, 0x40)
-    #self.set_register(self.__VL6180X_FIRMWARE_RESULT_SCALER, 0x01)
+      ## Additional settings defaults from community
+      #self.set_register(self.__VL6180X_SYSRANGE_MAX_CONVERGENCE_TIME, 0x32)
+      #self.set_register(
+      #    self.__VL6180X_SYSRANGE_RANGE_CHECK_ENABLES, 0x10 | 0x01)
+      #self.set_register_16bit(
+      #    self.__VL6180X_SYSRANGE_EARLY_CONVERGENCE_ESTIMATE, 0x7B)
+      #self.set_register_16bit(self.__VL6180X_SYSALS_INTEGRATION_PERIOD, 0x64)
+      #self.set_register(self.__VL6180X_SYSALS_ANALOGUE_GAIN, 0x40)
+      #self.set_register(self.__VL6180X_FIRMWARE_RESULT_SCALER, 0x01)
 
-    id = [self.get_register(self.__VL6180X_IDENTIFICATION_MODEL_ID + i) for i in range(0,8)]
-    if self.get_register(self.__VL6180X_SYSTEM_FRESH_OUT_OF_RESET) & 0x01 != 0:
-      self.set_register(self.__VL6180X_SYSTEM_FRESH_OUT_OF_RESET, 0x00)
-    self.set_register(self.__VL6180X_SYSRANGE_START, 0x03)
+      id = [self.get_register(self.__VL6180X_IDENTIFICATION_MODEL_ID + i) for i in range(0,8)]
+      if self.get_register(self.__VL6180X_SYSTEM_FRESH_OUT_OF_RESET) & 0x01 != 0:
+        self.set_register(self.__VL6180X_SYSTEM_FRESH_OUT_OF_RESET, 0x00)
+      self.set_register(self.__VL6180X_SYSRANGE_START, 0x03)
 
   cmd_GET_DISTANCE_help = "Get distance of object from sensor"
   def cmd_GET_DISTANCE(self, gcmd):
